43d2fb6efaadc9b1964baed3585416821c3a3656
Bertrand Dunogier <bertrand.dunogier@ez.no>
2016-10-20 01:15:53 +0200

    Prototyped QueryTypes integration
    { queries { latestContent { name } } }

--- GraphQL/Resolver/SearchResolver.php
+++ GraphQL/Resolver/SearchResolver.php
@@ -8,6 +8,7 @@ namespace BD\EzPlatformGraphQLBundle\GraphQL\Resolver;
 use eZ\Publish\API\Repository\SearchService;
 use eZ\Publish\API\Repository\Values\Content\Query;
 use eZ\Publish\API\Repository\Values\Content\Search\SearchHit;
+use eZ\Publish\Core\QueryType\QueryTypeRegistry;
 
 class SearchResolver
 {
@@ -16,9 +17,15 @@ class SearchResolver
      */
     private $searchService;
 
-    public function __construct(SearchService $searchService)
+    /**
+     * @var QueryTypeRegistry
+     */
+    private $queryTypeRegistry;
+
+    public function __construct(SearchService $searchService, QueryTypeRegistry $queryTypeRegistry)
     {
         $this->searchService = $searchService;
+        $this->queryTypeRegistry = $queryTypeRegistry;
     }
 
     public function searchContent($args)
@@ -44,6 +51,30 @@ class SearchResolver
         $query->filter = count($criteria) > 1 ? new Query\Criterion\LogicalAnd($criteria) : $criteria[0];
         $searchResult = $this->searchService->findContentInfo($query);
 
+        return $this->mapSearchHitsToValueObjects($searchResult);
+    }
+
+    /**
+     * Searches for content using a $queryTypeIdentifier with $args.
+     *
+     * Args are passed as-is to the QueryType.
+     *
+     * @return \eZ\Publish\API\Repository\Values\Content\Content[]
+     */
+    public function searchContentWithQueryType($queryTypeIdentifier, $args)
+    {
+        $queryType = $this->queryTypeRegistry->getQueryType($queryTypeIdentifier);
+        $searchResults = $this->searchService->findContentInfo($queryType->getQuery($args->getRawArguments()));
+
+        return $this->mapSearchHitsToValueObjects($searchResults);
+    }
+
+    /**
+     * @param $searchResult
+     * @return \eZ\Publish\API\Repository\Values\ValueObject[]
+     */
+    private function mapSearchHitsToValueObjects($searchResult)
+    {
         return array_map(
             function (SearchHit $hit) {
                 return $hit->valueObject;
